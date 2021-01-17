ac70e56ed0118a352fa760e6234b846f0fc1d648
Martinus Suherman <martinus.suherman@gmail.com>
2021-01-18 01:51:11 +0700

    Add file helper

---
+++ Misc/FileAndPathHelper.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+
+namespace Jokizilla.Api.Misc
+{
+    internal class FileAndPathHelper
+    {
+        public bool ValidateFileName(IFormFile file)
+        {
+            return file != null && !string.IsNullOrWhiteSpace(file.FileName);
+        }
+
+        public bool ValidateFileSize(IFormFile file, int maxSizeBytes)
+        {
+            return file.Length != 0 && file.Length <= maxSizeBytes;
+        }
+
+        public bool ValidateFileExtension(IFormFile file, string[] allowedExtensions)
+        {
+            string extension = Path.GetExtension(file.FileName).ToLowerInvariant();
+
+            return allowedExtensions.Length == 0 ||
+                (!string.IsNullOrWhiteSpace(extension) &&
+                allowedExtensions.Contains(extension));
+        }
+
+        public bool ValidateFileUrl(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url) ||
+                !Uri.IsWellFormedUriString(url, UriKind.Relative))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
