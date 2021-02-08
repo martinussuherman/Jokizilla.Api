4b0d1c8dd3abfaee65c61bcb0fec51edb7fd165c
Martinus Suherman <martinus.suherman@gmail.com>
2021-02-08 11:28:59 +0700

    Update ServiceController
    - Add xml comments
    - Fix EnableQuery options on Get to allow Expand
    - Fix Delete still refer to WorkLevels

--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -3,10 +3,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
 using AutoMapper.QueryableExtensions;
+using Jokizilla.Api.Misc;
 using Jokizilla.Models.Models;
 using Jokizilla.Models.ViewModels;
 using Microsoft.AspNet.OData;
-using Microsoft.AspNet.OData.Query;
 using Microsoft.AspNet.OData.Routing;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -16,18 +16,35 @@ using static Microsoft.AspNetCore.Http.StatusCodes;
 
 namespace Jokizilla.Api.Controllers
 {
+    /// <summary>
+    /// Represents a RESTful service of Service.
+    /// </summary>
     [Authorize]
     [Produces(JsonOutput)]
     [ODataRoutePrefix(nameof(Service))]
     [ApiVersionNeutral]
     public class ServiceController : ODataController
     {
+        /// <summary>
+        /// Service REST service.
+        /// </summary>
+        /// <param name="context">Database context.</param>
+        /// <param name="mapper">AutoMapper mapping profile.</param>
         public ServiceController(AppDbContext context, IMapper mapper)
         {
             _context = context;
             _mapper = mapper;
         }
 
+        /// <summary>
+        /// Retrieves list of Service.
+        /// </summary>
+        /// <remarks>
+        /// *Anonymous Access*
+        /// </remarks>
+        /// <returns>List of Service.</returns>
+        /// <response code="200">List of Service successfully retrieved.</response>
+        [AllowAnonymous]
         [ODataRoute]
         [ProducesResponseType(typeof(ODataValue<IEnumerable<ServiceViewDto>>), Status200OK)]
         [EnableQuery]
@@ -38,10 +55,21 @@ namespace Jokizilla.Api.Controllers
                 .ProjectTo<ServiceViewDto>(_mapper.ConfigurationProvider);
         }
 
+        /// <summary>
+        /// Gets a single Service.
+        /// </summary>
+        /// <remarks>
+        /// *Anonymous Access*
+        /// </remarks>
+        /// <param name="id">The requested Service identifier.</param>
+        /// <returns>The requested Service.</returns>
+        /// <response code="200">The Service was successfully retrieved.</response>
+        /// <response code="404">The Service does not exist.</response>
+        [AllowAnonymous]
         [ODataRoute(IdRoute)]
         [ProducesResponseType(typeof(ServiceViewDto), Status200OK)]
         [ProducesResponseType(Status404NotFound)]
-        [EnableQuery(AllowedQueryOptions = AllowedQueryOptions.Select)]
+        [EnableQuery]
         public SingleResult<ServiceViewDto> Get(ushort id)
         {
             return SingleResult.Create(
@@ -51,6 +79,19 @@ namespace Jokizilla.Api.Controllers
                     .ProjectTo<ServiceViewDto>(_mapper.ConfigurationProvider));
         }
 
+        /// <summary>
+        /// Creates a new Service.
+        /// </summary>
+        /// <remarks>
+        /// *Min role: Admin*
+        /// </remarks>
+        /// <param name="create">The Service to create.</param>
+        /// <returns>The created Service.</returns>
+        /// <response code="201">The Service was successfully created.</response>
+        /// <response code="204">The Service was successfully created.</response>
+        /// <response code="400">The Service is invalid.</response>
+        /// <response code="409">The Service with supplied id already exist.</response>
+        [MultiRoleAuthorize(RoleEnum.JokizillaAdmin)]
         [ProducesResponseType(typeof(ServiceViewDto), Status201Created)]
         [ProducesResponseType(Status204NoContent)]
         [ProducesResponseType(Status400BadRequest)]
@@ -86,6 +127,20 @@ namespace Jokizilla.Api.Controllers
                 .SingleAsync());
         }
 
+        /// <summary>
+        /// Updates an existing Service.
+        /// </summary>
+        /// <remarks>
+        /// *Min role: Admin*
+        /// </remarks>
+        /// <param name="id">The requested Service identifier.</param>
+        /// <param name="delta">The partial Service to update.</param>
+        /// <returns>The updated Service.</returns>
+        /// <response code="200">The Service was successfully updated.</response>
+        /// <response code="204">The Service was successfully updated.</response>
+        /// <response code="400">The Service is invalid.</response>
+        /// <response code="404">The Service does not exist.</response>
+        [MultiRoleAuthorize(RoleEnum.JokizillaAdmin)]
         [ODataRoute(IdRoute)]
         [ProducesResponseType(typeof(ServiceViewDto), Status200OK)]
         [ProducesResponseType(Status204NoContent)]
@@ -112,19 +167,30 @@ namespace Jokizilla.Api.Controllers
             return Updated(_mapper.Map<Service, ServiceUpdateDto>(item));
         }
 
+        /// <summary>
+        /// Deletes a Service.
+        /// </summary>
+        /// <remarks>
+        /// *Min role: Admin*
+        /// </remarks>
+        /// <param name="id">The Service to delete.</param>
+        /// <returns>None</returns>
+        /// <response code="204">The Service was successfully deleted.</response>
+        /// <response code="404">The Service does not exist.</response>
+        [MultiRoleAuthorize(RoleEnum.JokizillaAdmin)]
         [ODataRoute(IdRoute)]
         [ProducesResponseType(Status204NoContent)]
         [ProducesResponseType(Status404NotFound)]
         public async Task<IActionResult> Delete(ushort id)
         {
-            WorkLevel delete = await _context.WorkLevels.FindAsync(id);
+            Service delete = await _context.Services.FindAsync(id);
 
             if (delete == null)
             {
                 return NotFound();
             }
 
-            _context.WorkLevels.Remove(delete);
+            _context.Services.Remove(delete);
             await _context.SaveChangesAsync();
             return NoContent();
         }
